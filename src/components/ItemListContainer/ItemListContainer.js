6f75a2f22377ec2de6decfab55252bb86a9f65a1
Phoebe <phoebe.wilckens@gmail.com>
2021-05-19 21:05:54 -0400

    added an itemcount

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import ItemCount from "../ItemCount/ItemCount";
+import '../ItemCount/ItemCount.css';
 
 export default function ItemListContainer(){
+    const[cantidad, setCantidad] = useState(1);
+
+    const handleCantidad = (cantidad) => {
+        setCantidad(cantidad);
+    }
+
 return(
     <>
     <h1 style={{ textAlign: 'center' }}>Bienvenido/a !</h1>
+    <ItemCount stock={5} initial={1} onAdd={handleCantidad}/>
+       <div className="AddtoCart">
+            <button>Agregar al Carro ({cantidad})</button>
+        </div>
 </>
 )
 }
